5636734dce5e2630a23bf5eb9d96b5c86dcf05a3
Raghavendran <raghavedran@oath.com>
2018-10-21 02:16:38 -0500

    github

--- src/App.js
+++ src/App.js
@@ -2,6 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Header extends React.Component {
+   render() {
+      return (
+         <div className="App-header">
+            <h1>Raghavendra Nataraj</h1>
+         </div>
+      );
+   }
+}
+
+class MainPage extends React.Component {
+   render() {
+      return (
+         <div className="Body"> 
+	</div>
+      );
+   }
+}
+
+class Footer extends React.Component {
+   render() {
+      return (
+         <div className="App-footer">
+            <a href="https://github.com/raghavendra-nataraj" target="_blank"><img class="footer-icons" src={require('./Octocat.png')}/></a>
+         </div>
+      );
+   }
+}
+
 class App extends Component {
 
   componentDidMount(){
@@ -11,9 +40,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        <h1>Raghavendra Nataraj</h1>
-	</header>
+        <Header></Header>
+        <MainPage></MainPage>
+	<Footer></Footer>
       </div>
     );
   }
